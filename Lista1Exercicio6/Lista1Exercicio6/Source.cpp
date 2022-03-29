69ca1f0da0e456956904a319ad6e74c2f77e5fa8
Gabriel Bittencourt Gamboa <gabrielbgamboa@hotmail.com>
2022-03-28 23:23:58 -0300

    Finalizado exercicio 6

--- Lista1Exercicio6/Lista1Exercicio6/Source.cpp
+++ Lista1Exercicio6/Lista1Exercicio6/Source.cpp
@@ -13,13 +13,17 @@ using namespace std;
 
 const float PI = 3.14159;
 
+char exercicio = '0';
+
+int nPoints;
+
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
-void drawArray(const GLuint& VAO);
+void drawArray(const GLuint& VAO, GLint colorLoc);
 
 GLuint setupGeometry();
 
-GLfloat* generateCircle(float radius, int nPoints, int totalSize);
+GLfloat* generateCircle(float radius, int totalSize);
 
 const GLuint& setupVao();
 
@@ -29,9 +33,26 @@ const GLuint WIDTH = 800, HEIGHT = 600;
 
 int main() {
 
+	switch (exercicio) {
+
+		case '0':
+		case 'C':
+		case 'D':
+			nPoints = 50;
+			break;
+
+		case 'A':
+			nPoints = 8;
+			break;
+
+		case 'B':
+			nPoints = 5;
+			break;
+	}
+
 	glfwInit();
 
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Ola Triangulo! - Gabriel Bittencourt", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Lista 1 Exercicio 6 - Gabriel Bittencourt", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
 	glfwSetKeyCallback(window, key_callback);
@@ -39,9 +60,6 @@ int main() {
 	if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 	}
-
-	cout << "Renderer: " << glGetString(GL_RENDERER) << endl;
-	cout << "OpenGL version supported " << glGetString(GL_VERSION) << endl;
 
 	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
@@ -51,13 +69,16 @@ int main() {
 
 	GLuint VAO = setupGeometry();
 	
+	GLint colorLoc = glGetUniformLocation(shader.ID, "inputColor");
+	assert(colorLoc > -1);
+
 	glUseProgram(shader.ID);
 	
 	while (!glfwWindowShouldClose(window)) {
 
 		glfwPollEvents();
 
-		drawArray(VAO);
+		drawArray(VAO, colorLoc);
 
 		glfwSwapBuffers(window);
 	}
@@ -82,17 +103,17 @@ GLuint setupGeometry() {
 	return setupVao();
 }
 
-GLfloat* generateCircle(float radius, int nPoints, int totalSize) {
+GLfloat* generateCircle(float radius, int totalSize) {
 
 
 	GLfloat* vertices = new GLfloat[totalSize];
 
+	float angle = 0.0;
+	float slice = 2 * PI / (GLfloat) nPoints;
+
 	vertices[0] = 0.0;
 	vertices[1] = 0.0;
 	vertices[2] = 0.0;
-
-	float angle = 0.0;
-	float slice = 2 * PI / (float) nPoints;
 
 	for (int i = 3; i < totalSize; i += 3, angle += slice) {
 
@@ -100,8 +121,6 @@ GLfloat* generateCircle(float radius, int nPoints, int totalSize) {
 		vertices[i + 1] = radius * sin(angle);
 		vertices[i + 2] = 0.0;
 	}
-	
-	cout << "Teste:" << vertices;
 
 	return vertices;
 }
@@ -109,10 +128,9 @@ GLfloat* generateCircle(float radius, int nPoints, int totalSize) {
 void setupVbo() {
 
 	float radius = 0.5;
-	int nPoints = 30;
-	int totalSize = (nPoints + 1) * 6;
+	int totalSize = (nPoints + 2) * 3;
 
-	GLfloat* vertices = generateCircle(radius, nPoints, totalSize);
+	GLfloat* vertices = generateCircle(radius, totalSize);
 
 	GLuint VBO;
 
@@ -120,7 +138,7 @@ void setupVbo() {
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-	glBufferData(GL_ARRAY_BUFFER, sizeof(totalSize), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, totalSize * sizeof(totalSize), vertices, GL_STATIC_DRAW);
 }
 
 const GLuint& setupVao() {
@@ -140,16 +158,33 @@ const GLuint& setupVao() {
 	return VAO;
 }
 
-void drawArray(const GLuint& VAO) {
+void drawArray(const GLuint& VAO, GLint colorLoc) {
+
 	glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glLineWidth(10);
 	glPointSize(10);
 
+	glUniform4f(colorLoc, 1.0f, 0.0f, 0.0f, 1.0f);
 	glBindVertexArray(VAO);
+	
+	switch (exercicio) {
 
-	glDrawArrays(GL_LINE_LOOP, 0, 29);
+		case '0':
+		case 'A':
+		case 'B':
+			glDrawArrays(GL_TRIANGLE_FAN, 0, nPoints + 2);
+			break;
+
+		case 'C':
+			glDrawArrays(GL_TRIANGLE_FAN, 0, 45);
+			break;
+
+		case 'D':
+			glDrawArrays(GL_TRIANGLE_FAN, 0, 10);
+			break;
+	}
 
 	glBindVertexArray(0);
 }
